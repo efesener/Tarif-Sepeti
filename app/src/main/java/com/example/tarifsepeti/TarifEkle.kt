401012b42a0425b7b7865560517f685e6467a8e2
sude-1 <69909424+sude-1@users.noreply.github.com>
2021-05-24 22:28:57 +0300

    Tarid ekle deneme

--- app/src/main/java/com/example/tarifsepeti/TarifEkle.kt
+++ app/src/main/java/com/example/tarifsepeti/TarifEkle.kt
@@ -7,5 +7,6 @@ class TarifEkle : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tarif_ekle)
+        println("sude")
     }
 }
