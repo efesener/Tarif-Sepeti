0db2593aed5b0a5f144585c58b58701cf1986ebc
MerveKaragoz1 <mervekaragoz13@gmail.com>
2021-05-28 21:32:21 +0300

    TarifDetay changed.

--- app/src/main/java/com/example/tarifsepeti/TarifDetay.kt
+++ app/src/main/java/com/example/tarifsepeti/TarifDetay.kt
@@ -1,15 +1,22 @@
 package com.example.tarifsepeti
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_tarif_detay.*
+import kotlinx.android.synthetic.main.activity_tarif_ekle.*
 import kotlinx.android.synthetic.main.activity_tarif_listele.*
+import java.lang.Exception
 
 class TarifDetay : AppCompatActivity() {
+
+
+
     // @@@@@@ kullanıcı adını global değişken olarak tanımlıyorum ki her fonksiyon içinde kullanabileyim
     var  kullaniciAdi = ""
     // @@@@@@ kullanıcı adını global değişken olarak tanımlıyorum ki her fonksiyon içinde kullanabileyim
@@ -18,16 +25,37 @@ class TarifDetay : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tarif_detay)
 
+
+
         // @@@@@@ Bir önceki sayfadan gelen kullaniciAdi bilgisini alıyoruz
         val intent = intent
         kullaniciAdi = intent.getStringExtra("kullaniciAdi").toString()
 
-        txtKullaniciAdi2.text = kullaniciAdi
+        //txtKullaniciAdi2.text = kullaniciAdi
         // @@@@@@ Bir önceki sayfadan gelen kullaniciAdi bilgisini alıyoruz
 
-        println("merve");
+        detay()
+
     }
 
+    fun detay() {
+        try{
+            val database = this.openOrCreateDatabase("Yemekler",Context.MODE_PRIVATE,null)
+            val cursor = database.rawQuery("SELECT * FROM yemekler WHERE id=12345",null) //WHERE
+
+            val yemekIsmiIndex = cursor.getColumnIndex("yemekIsmi")
+
+            while (cursor.moveToNext()){
+                yemekIsim.text= cursor.getString(yemekIsmiIndex).toString()
+                }
+
+            cursor.close()
+
+        }
+        catch(e: Exception){
+            e.printStackTrace()
+        }
+    }
     // @@@@@@ MENU ILE İLGİLİ FONKSİYONLAR
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         super.onCreateOptionsMenu(menu)
